e921c51fa292845f7c5b30ab4f9ce93394d48ed3
邱允柔 <cys20030203@gmail.com>
2025-03-27 14:19:31 +0800

    feat: 3-2 ImageList

---
+++ src/Ch3-integration-testing/3-2-3-3/ImageList/ImageList.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import ImageItem from "./ImageItem";
+
+const ImageList = () => {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("https://dummyjson.com/products");
+            const data = await response.json();
+            setImages(data.products);
+
+        }
+        fetchData();
+    }, []);
+
+    const renderImages = () => {
+        return images.map(({ id, title, image }) => (
+            <ImageItem key={id} title={title} image={images[0]} />
+        ));
+    };
+
+    return (
+        <>
+            {images.length !== 0 ? (
+                renderImages()
+            ) : (
+                <div data-test-id="no-data-prompt">No data to display.</div>
+            )}
+        </>
+    )
+}
+
+export default ImageList;
